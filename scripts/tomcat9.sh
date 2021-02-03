b923cbbf31d61a01a070e875b973d592da341b47
Rudolf <69104895+rudolf8@users.noreply.github.com>
2021-02-03 21:15:02 +0100

    Update tomcat9.sh

--- scripts/tomcat9.sh
+++ scripts/tomcat9.sh
@@ -34,3 +34,23 @@ sed -i 's/After=syslog.target network.target/After=syslog.target network.target 
 #and the provides name to match script name
 sed -i 's/Provides:          tomcat9/Provides:          tomcat@oxar/' /etc/init.d/${TOMCAT_OXAR_SERVICE_NAME}
 
+#Reload systemd just in case anything is cached with this service name
+systemctl daemon-reload
+#Stop and dissable tomcat from sytem startup
+systemctl stop ${TOMCAT_SERVICE_NAME}
+systemctl disable ${TOMCAT_SERVICE_NAME}
+
+#Add a user into tomcat-users.xml (/etc/tomcat/tomcat-user.xml) as defined in config.properties
+perl -i -p -e "s/<tomcat-users>/<tomcat-users>\n  <\!-- Auto generated content by http\:\/\/www.github.com\/OraOpenSource\/oraclexe-apex install scripts -->\n  <role rolename=\"manager-gui\"\/>\n  <user username=\"${OOS_TOMCAT_USERNAME}\" password=\"${OOS_TOMCAT_PASSWORD}\" roles=\"manager-gui\"\/>\n  <\!-- End auto-generated content -->/g" ${CATALINA_HOME}/conf/tomcat-users.xml
+
+# Copy the configuration template over
+cp -f ${CATALINA_HOME}/conf/server.xml ${CATALINA_HOME}/conf/server_original.xml
+# See #150
+\cp -f $OOS_SOURCE_DIR/tomcat/server.xml ${CATALINA_HOME}/conf/server.xml
+
+# Set the preferred port
+sed -i "s/OOS_TOMCAT_SERVER_PORT/${OOS_TOMCAT_PORT}/" ${CATALINA_HOME}/conf/server.xml
+
+systemctl enable ${TOMCAT_OXAR_SERVICE_NAME}
+systemctl start ${TOMCAT_OXAR_SERVICE_NAME}
+
