921074f6c661994370d6d1d58be4ff73438a752d
Rudolf <69104895+rudolf8@users.noreply.github.com>
2021-02-03 20:45:01 +0100

    Update tomcat9.sh

--- scripts/tomcat9.sh
+++ scripts/tomcat9.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Create user and home directory
 useradd -m -U -d /opt/tomcat -s /bin/false tomcat
 
 cd $OOS_SOURCE_DIR/tmp
 ${OOS_UTILS_DIR}/download.sh $OOS_TOMCAT_FILE_URL
 
-tar -xf $OOS_TOMCAT_FILE_URL
+unzip $OOS_TOMCAT_ZIP_FILENAME
 
 #Make dir if doesn't exist
 rm -rf /opt/tomcat
+
 cp -r apache* /opt/tomcat
 
 ln -s /opt/tomcat/$OOS_TOMCAT_FILENAME /opt/tomcat/latest
