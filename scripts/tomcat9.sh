68b08a7f1c02e870b4c2597e94fe8566acdbd947
Rudolf <69104895+rudolf8@users.noreply.github.com>
2021-02-04 10:33:57 +0100

    Update tomcat9.sh

--- scripts/tomcat9.sh
+++ scripts/tomcat9.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
-# UNDER CONSTRUCTION
+### Install Tomcat 9 on Ubuntu, Debian, CentOS, OpenSUSE 64Bits
 
-# Create user and home directory
-useradd -m -U -d /opt/tomcat -s /bin/false tomcat
+# Check if user has root privileges
+if [[ $EUID -ne 0 ]]; then
+echo "You must run the script as root or using sudo"
+   exit 1
+fi
+
+rm -rf /usr/local/tomcat
+groupadd tomcat && useradd -M -s /bin/nologin -g tomcat -d /usr/local/tomcat tomcat
 
 cd $OOS_SOURCE_DIR/tmp
 ${OOS_UTILS_DIR}/download.sh $OOS_TOMCAT_FILE_URL
 
 unzip $OOS_TOMCAT_ZIP_FILENAME
 
-#Make dir if doesn't exist
-rm -rf /opt/tomcat
+cd apache-tomcat* 
+cp -r * /usr/local/tomcat
 
-cp -r apache* /opt/tomcat
+ln -s /usr/local/tomcat /usr/local/tomcat/latest
+ 
+echo 'JAVA_HOME=/usr/local/java
+export JAVA_HOME
+PATH=$PATH:$JAVA_HOME/bin
+export PATH' >> /etc/profile
+ 
+source /etc/profile
+java -version
 
-ln -s /opt/tomcat/$OOS_TOMCAT_FILENAME /opt/tomcat/latest
-
-# Make tomcat dir owner
-chown -R tomcat: /opt/tomcat
-
-sh -c 'chmod +x /opt/tomcat/latest/bin/*.sh'
-
-. /opt/tomcat/latest/tomcat.conf
-TOMCAT_USER=tomcat
-TOMCAT_SERVICE_NAME=tomcat
-
-
-cp /etc/systemd/system/tomcat.service /etc/systemd/system/${TOMCAT_OXAR_SERVICE_NAME}.service
-#cp /usr/lib/systemd/system/${TOMCAT_SERVICE_NAME}.service /usr/lib/systemd/system/${TOMCAT_OXAR_SERVICE_NAME}.service
-
-sed -i 's/After=syslog.target network.target/After=syslog.target network.target oracle-xe.service/' /etc/systemd/system/${TOMCAT_OXAR_SERVICE_NAME}.service
-#sed -i 's/After=syslog.target network.target/After=syslog.target network.target oracle-xe.service/' /usr/lib/systemd/system/${TOMCAT_OXAR_SERVICE_NAME}.service
-
-#and the provides name to match script name
-sed -i 's/Provides:          tomcat9/Provides:          tomcat@oxar/' /etc/init.d/${TOMCAT_OXAR_SERVICE_NAME}
-
-#Reload systemd just in case anything is cached with this service name
+echo '# Systemd unit file for tomcat
+[Unit]
+Description=Apache Tomcat Web Application Container
+After=syslog.target network.target
+[Service]
+Type=forking
+Environment=JAVA_HOME=/usr/local/java
+Environment=CATALINA_PID=/usr/local/tomcat/temp/tomcat.pid
+Environment=CATALINA_HOME=/usr/local/tomcat
+Environment=CATALINA_BASE=/usr/local/tomcat
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
+Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
+ExecStart=/usr/local/tomcat/bin/startup.sh
+ExecStop=/bin/kill -15 $MAINPID
+User=tomcat
+Group=tomcat
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/${TOMCAT_OXAR_SERVICE_NAME}
+ 
+ 
 systemctl daemon-reload
-#Stop and dissable tomcat from sytem startup
-systemctl stop ${TOMCAT_SERVICE_NAME}
-systemctl disable ${TOMCAT_SERVICE_NAME}
-
-#Add a user into tomcat-users.xml (/etc/tomcat/tomcat-user.xml) as defined in config.properties
-perl -i -p -e "s/<tomcat-users>/<tomcat-users>\n  <\!-- Auto generated content by http\:\/\/www.github.com\/OraOpenSource\/oraclexe-apex install scripts -->\n  <role rolename=\"manager-gui\"\/>\n  <user username=\"${OOS_TOMCAT_USERNAME}\" password=\"${OOS_TOMCAT_PASSWORD}\" roles=\"manager-gui\"\/>\n  <\!-- End auto-generated content -->/g" ${CATALINA_HOME}/conf/tomcat-users.xml
-
-# Copy the configuration template over
-cp -f ${CATALINA_HOME}/conf/server.xml ${CATALINA_HOME}/conf/server_original.xml
-# See #150
-\cp -f $OOS_SOURCE_DIR/tomcat/server.xml ${CATALINA_HOME}/conf/server.xml
-
-# Set the preferred port
-sed -i "s/OOS_TOMCAT_SERVER_PORT/${OOS_TOMCAT_PORT}/" ${CATALINA_HOME}/conf/server.xml
-
-systemctl enable ${TOMCAT_OXAR_SERVICE_NAME}
 systemctl start ${TOMCAT_OXAR_SERVICE_NAME}
+systemctl enable ${TOMCAT_OXAR_SERVICE_NAME}
 
+## Open in web browser:
+## http://server_IP_address:8080
