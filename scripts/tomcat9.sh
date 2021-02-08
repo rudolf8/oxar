a74c316d927ce17d166b169fac174f3fb98b4847
Rudolf <69104895+rudolf8@users.noreply.github.com>
2021-02-08 11:09:42 +0100

    Update tomcat9.sh

--- scripts/tomcat9.sh
+++ scripts/tomcat9.sh
@@ -20,34 +20,43 @@ cp -r * /usr/local/tomcat
 
 ln -s /usr/local/tomcat /usr/local/tomcat/latest
  
-echo 'JAVA_HOME=/usr/local/java
-export JAVA_HOME
-PATH=$PATH:$JAVA_HOME/bin
-export PATH' >> /etc/profile
+#echo 'JAVA_HOME=/usr/local/java
+#export JAVA_HOME
+#PATH=$PATH:$JAVA_HOME/bin
+#export PATH' >> /etc/profile
  
-source /etc/profile
-java -version
+#source /etc/profile
+#java -version
 
-echo '# Systemd unit file for tomcat
-[Unit]
-Description=Apache Tomcat Web Application Container
-After=syslog.target network.target
-[Service]
-Type=forking
-Environment=JAVA_HOME=/usr/local/java
-Environment=CATALINA_PID=/usr/local/tomcat/temp/tomcat.pid
-Environment=CATALINA_HOME=/usr/local/tomcat
-Environment=CATALINA_BASE=/usr/local/tomcat
-Environment='CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC'
-Environment='JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom'
-ExecStart=/usr/local/tomcat/bin/startup.sh
-ExecStop=/bin/kill -15 $MAINPID
-User=tomcat
-Group=tomcat
-[Install]
-WantedBy=multi-user.target' > /etc/systemd/system/${TOMCAT_OXAR_SERVICE_NAME}
+# Set tomcat environmental variables such as CATALINA_HOME
+. /etc/tomcat/tomcat.conf
+TOMCAT_USER=tomcat
+TOMCAT_SERVICE_NAME=tomcat
+
+    
+ #Modifications to tomcat service file.
+ cp $OOS_SOURCE_DIR/tomcat/tomcat.service /etc/systemd/system/tomcat/${TOMCAT_OXAR_SERVICE_NAME}.service
  
- 
+ #Add `oracle-xe` to the After clause to encourage waiting for the db to be up and running
+ sed -i 's/After=syslog.target network.target/After=syslog.target network.target oracle-xe.service/' /etc/systemd/system/tomcat/${TOMCAT_OXAR_SERVICE_NAME}.service
+
+#Reload systemd just in case anything is cached with this service name
+systemctl daemon-reload
+#Stop and dissable tomcat from sytem startup
+systemctl stop ${TOMCAT_SERVICE_NAME}
+systemctl disable ${TOMCAT_SERVICE_NAME}
+
+#Add a user into tomcat-users.xml (/etc/tomcat/tomcat-user.xml) as defined in config.properties
+perl -i -p -e "s/<tomcat-users>/<tomcat-users>\n  <\!-- Auto generated content by http\:\/\/www.github.com\/OraOpenSource\/oraclexe-apex install scripts -->\n  <role rolename=\"manager-gui\"\/>\n  <user username=\"${OOS_TOMCAT_USERNAME}\" password=\"${OOS_TOMCAT_PASSWORD}\" roles=\"manager-gui\"\/>\n  <\!-- End auto-generated content -->/g" ${CATALINA_HOME}/conf/tomcat-users.xml
+
+# Copy the configuration template over
+cp -f ${CATALINA_HOME}/conf/server.xml ${CATALINA_HOME}/conf/server_original.xml
+# See #150
+cp -f $OOS_SOURCE_DIR/tomcat/server.xml ${CATALINA_HOME}/conf/server.xml
+
+# Set the preferred port
+sed -i "s/OOS_TOMCAT_SERVER_PORT/${OOS_TOMCAT_PORT}/" ${CATALINA_HOME}/conf/server.xml
+
 systemctl daemon-reload
 systemctl start ${TOMCAT_OXAR_SERVICE_NAME}
 systemctl enable ${TOMCAT_OXAR_SERVICE_NAME}
