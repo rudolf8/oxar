7b0ed286cf54c1cf85dd8cfb9a9e0f5c865743c6
Rudolf <69104895+rudolf8@users.noreply.github.com>
2021-02-15 13:12:25 +0100

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+if [ -n "$(command -v yum)" ]; then
+  echo; echo \* Installing Ngix with yum \*
+  
+  yum install nginx -y
+
+elif [ -n "$(command -v apt-get)" ]; then
+  echo; echo \* Installing Ngix with apt-get \*
+ 
+ apt-get install nginx -y
+ 
+else
+  echo; echo \* No known package manager found \*
+fi
+
+systemctl start nginx
+systemctl enable nginx
+
+# Create SSL certificate
+mkdir /etc/ssl/private
+chmod 700 /etc/ssl/private
+
+#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
