b46658efa64cae9877c0c903613243c59bde0c64
Rudolf <69104895+rudolf8@users.noreply.github.com>
2021-02-15 13:32:23 +0100

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -17,8 +17,20 @@ fi
 systemctl start nginx
 systemctl enable nginx
 
-# Create SSL certificate
+# Generate an unsigned certificate
+
 mkdir /etc/ssl/private
 chmod 700 /etc/ssl/private
+cd /etc/ssl/private
+
+openssl req -newkey rsa:2048 -nodes -keyout /etc/ssl/private/nginx-selfsigned.key \
+  -x509 -days 365 -out /etc/ssl/certs/nginx-selfsigned.crt \
+  -subj "/C=DE/ST=Bavaria/L=Munich/O=Dis/CN=localhost"
+
+# Modify Node4ORDS Config
+cd /opt/node4ords
+sed -i 's/CHANGEME_HTTPS_KEYPATH/\/var\/www\/certs\/localhost.key/g' config.js
+sed -i 's/CHANGEME_HTTPS_CERTPATH/\/var\/www\/certs\/localhost.crt/g' config.js
+sed -i 's/config.web.https.enabled = false;/config.web.https.enabled = true;/g' config.js
 
 #openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
